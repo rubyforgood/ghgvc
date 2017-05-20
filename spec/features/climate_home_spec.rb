f3995b6856b64dda036a9d2088bbc1afd867cb73
Soren Essen <sorenessen@gmail.com>
2017-05-20 10:24:09 -0400

    spec modifications

--- spec/features/climate_home_spec.rb
+++ spec/features/climate_home_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe "Climate Home", type: :feature do
+RSpec.describe "Climate Home" do
 
   describe "visit path" do
     before do
@@ -7,6 +7,17 @@ RSpec.describe "Climate Home", type: :feature do
     end
     it "should display root path" do
       expect(page).to have_content("Ecosystem Climate Regulation Services Calculator")
+    end
+  end
+
+  describe "Running Calaculator", js: true do
+  	context "Running Calaculator With No Selected Ecosystem" do
+  	  before do
+  		find('#run.ghgvc.calculator').click
+  	  end
+  	  it "should fail to calculate" do
+  		expect(page).to_not have_content('ok')
+      end
     end
   end
 end
