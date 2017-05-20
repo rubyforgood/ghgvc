a5257c33a565af91b2842e2f1cdcdb422c4383bf
Soren Essen <sorenessen@gmail.com>
2017-05-20 11:13:36 -0400

    climate_home_spec fix

--- spec/features/climate_home_spec.rb
+++ spec/features/climate_home_spec.rb
@@ -3,22 +3,22 @@ RSpec.describe "Climate Home" do
 
   describe "visit path" do
     before do
-  	  visit root_path
+      visit root_path
     end
 
     it "should display root path" do
       expect(page).to have_content("Ecosystem Climate Regulation Services Calculator")
     end
   end
-
   describe "Running Calaculator", js: true do
-  	context "Running Calaculator With No Selected Ecosystem" do
-  	  before do
-  		find('#run.ghgvc.calculator').click
-  	  end
-  	  
-  	  it "should fail to calculate" do
-  		expect(page).to_not have_content('ok')
+    context "Running Calaculator With No Selected Ecosystem" do
+      before do
+        visit root_path
+        find('#run.ghgvc.calculator').click
+      end
+
+      it "should fail to calculate" do
+        expect(page).to_not have_content('ok')
       end
     end
   end
